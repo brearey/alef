76a584b188b08ce590768e77bdcc8f5a7d8b5c1b
brearey <brearey4@gmail.com>
2024-05-03 09:35:53 +0900

    Init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Инциализация документации API на основе Swagger
+  const swaggerConfig = new DocumentBuilder().setTitle('Alef app').build()
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, swaggerDoc)
+
   await app.listen(3000);
 }
 bootstrap();
